a5ac4fd8606dcd6a153a49f6325c2fe80d19c15e
Chris Birchall <chris.birchall@47deg.com>
2020-05-19 15:03:00 +0100

    Move some details into a Helpers object

--- src/main/scala/mylibrary/Functor.scala
+++ src/main/scala/mylibrary/Functor.scala
@@ -20,32 +20,59 @@ object Functor {
     def [A, B] (t: T).map(f: A => B): T = t
   }
 
-  type Derived[F[_]] = DerivedFunctor[Functor[F]]
-  type ProductDerived[F[_]] = DerivedProductFunctor[Functor[F]]
-  type CoproductDerived[F[_]] = DerivedCoproductFunctor[Functor[F]]
+  object Helpers {
 
-  inline def summonAsArray[T <: Tuple]: Array[Any] =
-    summonAsArray0[T](0, new Array[Any](constValue[Tuple.Size[T]]))
+    inline def summonAsArray[T <: Tuple]: Array[Any] =
+      summonAsArray0[T](0, new Array[Any](constValue[Tuple.Size[T]]))
 
-  inline def summonAsArray0[T](i: Int, arr: Array[Any]): Array[Any] = inline erasedValue[T] match {
-    case _: Unit => arr
-    case _: (a *: b) =>
-      arr(i) = summonInline[a]
-      summonAsArray0[b](i+1, arr)
+    inline def summonAsArray0[T](i: Int, arr: Array[Any]): Array[Any] = inline erasedValue[T] match {
+      case _: Unit => arr
+      case _: (a *: b) =>
+        arr(i) = summonInline[a]
+        summonAsArray0[b](i+1, arr)
+    }
+
+    final class ArrayProduct(val elems: Array[Any]) extends Product {
+      def canEqual(that: Any): Boolean = true
+      def productElement(n: Int) = elems(n)
+      def productArity = elems.length
+      override def productIterator: Iterator[Any] = elems.iterator
+    }
+
+    type Derived[F[_]] = DerivedFunctor[Functor[F]]
+    type ProductDerived[F[_]] = DerivedProductFunctor[Functor[F]]
+    type CoproductDerived[F[_]] = DerivedCoproductFunctor[Functor[F]]
+
+    type PolyMirror[C, O[_]] = C { type MirroredType = O ; type MirroredElemTypes[_] }
+    type MirrorOf[O[_]] = PolyMirror[Mirror, O]
+    type ProductMirrorOf[O[_]] = PolyMirror[Mirror.Product, O]
+    type CoproductMirrorOf[O[_]] = PolyMirror[Mirror.Sum, O]
+
+    case class Wrap[T](t: T)
+    class Dummy
+    type Apply[F[_]] = F[Dummy]
+    type Unapply[T] = T match {
+      case Wrap[Apply[a]] => Functor[a]
+      case Wrap[Dummy] => Functor[Id]
+      case Wrap[c] => Functor[Const[c]]
+    }
+
+    type Functors[T[_]] = Functors0[Apply[T]]
+
+    type Functors0[T] <: Tuple = T match {
+      case Unit => Unit
+      case (a *: b) => Unapply[Wrap[a]] *: Functors0[b]
+    }
+
   }
+
+  import Helpers._
 
   abstract class DerivedFunctor[FuncF] {
     def erasedMap(fa: Any)(f: Any => Any): Any
 
     def map[F[_], A, B](fa: F[A])(f: A => B): F[B] =
       erasedMap(fa)(f.asInstanceOf).asInstanceOf
-  }
-
-  private final class ArrayProduct(val elems: Array[Any]) extends Product {
-    def canEqual(that: Any): Boolean = true
-    def productElement(n: Int) = elems(n)
-    def productArity = elems.length
-    override def productIterator: Iterator[Any] = elems.iterator
   }
 
   final class DerivedProductFunctor[FuncF](mirror: Mirror.Product, elemFunctors: Array[Any]) extends DerivedFunctor[FuncF] {
@@ -86,38 +113,17 @@ object Functor {
       new DerivedCoproductFunctor[Functor[F]](mirror, summonAsArray[E])
   }
 
-  type PolyMirror[C, O[_]] = C { type MirroredType = O ; type MirroredElemTypes[_] }
-  type MirrorOf[O[_]] = PolyMirror[Mirror, O]
-  type ProductMirrorOf[O[_]] = PolyMirror[Mirror.Product, O]
-  type CoproductMirrorOf[O[_]] = PolyMirror[Mirror.Sum, O]
-
   inline given mkDerived[F[_]](using mirror: MirrorOf[F]) as Derived[F] =
     inline mirror match {
       case p: ProductMirrorOf[F] => mkProductDerived[F](using p)
       case c: CoproductMirrorOf[F] => mkCoproductDerived[F](using c)
     }
 
-  case class Wrap[T](t: T)
-  class Dummy
-  type Apply[F[_]] = F[Dummy]
-  type Unapply[T] = T match {
-    case Wrap[Apply[a]] => Functor[a]
-    case Wrap[Dummy] => Functor[Id]
-    case Wrap[c] => Functor[Const[c]]
-  }
-
-  type LiftP[T[_]] = LiftP0[Apply[T]]
-
-  type LiftP0[T] <: Tuple = T match {
-    case Unit => Unit
-    case (a *: b) => Unapply[Wrap[a]] *: LiftP0[b]
-  }
-
   inline given mkProductDerived[F[_]](using mirror: ProductMirrorOf[F]) as ProductDerived[F] =
-    DerivedProductFunctor[F, LiftP[mirror.MirroredElemTypes]](mirror)
+    DerivedProductFunctor[F, Functors[mirror.MirroredElemTypes]](mirror)
 
   inline given mkCoproductDerived[F[_]](using mirror: CoproductMirrorOf[F]) as CoproductDerived[F] =
-    DerivedCoproductFunctor[F, LiftP[mirror.MirroredElemTypes]](mirror)
+    DerivedCoproductFunctor[F, Functors[mirror.MirroredElemTypes]](mirror)
 
   given derived[F[_]](using derivedFunctor: => Derived[F]) as Functor[F] {
     def [A, B] (fa: F[A]).map(f: A => B): F[B] =
